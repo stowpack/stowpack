db1243e88b2af925825b162f0e704703d55ecc17
TylerMS887 <115214762+TylerMS887@users.noreply.github.com>
2023-11-14 17:00:30 +0000

    Create initpkg.sh

---
+++ initpkg.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# This script provides APIs to manipulate the yumyum package manager.
+# See the online documentation for details.
+
+function yumyum-install {
+  for i in $@; do
+    basename="$(basename $i)"
+    install -C $i $YUMYUM_HOME/$basename
+    if $VERBOSE; then
+      echo "Installed: $i\t>\t$YUMYUM_HOME/$basename"
+    fi
+  done
+}
+
+function yumyum-delete {
+  for i in $@; do
+    rm $YUMYUM_HOME/$i
+   if $VERBOSE; then
+        echo "Deleted: \t\t$YUMYUM_HOME/$i"
+   fi
+  done
+}
+
+function yumyum-force-install {
+  for i in $@; do
+    basename="$(basename $i)"
+    install $i $YUMYUM_HOME/$basename
+    if $VERBOSE; then
+      echo "Force-installed: $i\t>\t$YUMYUM_HOME/$basename"
+    fi
+  done
+}
