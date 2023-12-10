ce64f0ef15c4b92b1057c28f549c17147e15ccdf
TylerMS887 <tylermageeshields@gmail.com>
2023-12-10 16:56:32 +0000

    lol

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+clear
 function stowpack_config {
   echo "# Configuration for stowpack. Read https://tylerms887.github.io/stowpack/config"
   echo "# for more information on using stowpack configuration."
@@ -9,16 +10,32 @@ function stowpack_config {
   echo "# display help info, but return an error."
   echo "stowpack_require_command: false"
 }
-echo "* Creates ~/.stowpack, ~/.stowpack/bin, ~/.stowpack/sbin"
-echo "* Clones the Stowpack repository to ~/.s/source/stowpack"
-echo "* Links ~/.s/s/stowpack/stowpack.sh to ~/.s/b/stowpack"
-echo "Please press Ctrl+C to cancel the installation."
-secs=10
+function cleanup() {
+    tput cnorm
+}
+
+trap cleanup EXIT
+echo "                  Welcome to"
+echo "  -_-/    ,                                ,,   
+ (_ /    ||        ;              _        ||   
+(_ --_  =||=  /'\\\\ \\\\/\\/\\ -_-_   < \\,  _-_ ||/\\ 
+  --_ )  ||  || || || | | || \\\\  /-|| ||   ||_< 
+ _/  ))  ||  || || || | | || || (( || ||   || | 
+(_-_-    \\\\, \\\\,/  \\\\/\\\\/ ||-'   \\/\\\\ \\\\,/ \\\\,\\ 
+                          |/                    
+                          '                     "
+echo "This is a complex process. While it definitely won't brick your system"
+echo "it will likely affect usage of existing software bundled with it."
+secs=15
+stty -echo
+tput civis
 while [ $secs -gt 0 ]; do
-   echo -ne "$secs\033[0K\r"
+   echo -ne "Press Ctrl+C to cancel before: $secs sec\033[0K\r"
    sleep 1
    : $((secs--))
 done
+stty echo
+tput cnorm
 # Make a folder for stowpack
 echo "==> Creating Stowpack folders..."
 mkdir -p ~/.stowpack/source
@@ -35,8 +52,17 @@ chmod +x ~/.stowpack/source/stowpack/stowpack.sh
 echo "==> Making a Stowpack configuration file..."
 stowpack_config > ~/.stowpack/config
 # Install stowpack-provided bash
-echo "==> Installing Stowpack's bash..."
-$BASH ~/.stowpack/stowpack.sh install stowbash
+mkdir -p ~/.stowpack/bowls
+if [ ! -d "~/.stowpack/bowls/main" ]; then
+  echo "==> Cooking main bowl."
+  if git clone https://github.com/stowpack/main ~/.stowpack/bowls/main; then
+  echo "Cooked!"
+  echo "==> Installing Stowpack's bash..."
+  $BASH ~/.stowpack/bin/stowpack install stowbash
+  else
+  echo "Error. Continuing without a main bowl."
+  fi
+fi
 # Print a success message
 echo "==> Installation completed!"
 echo "Next steps:"
--- stowpack.sh
+++ stowpack.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-mkdir -p ~/.stowpack
-
 # A basic package manager in pure bash
 BOLD=$(tput bold)
 DIM=$(tput dim)
@@ -11,41 +9,143 @@ RESET=$(tput sgr0)
 function parse_yaml {
    local prefix=$2
    local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
+   sed -ne "s|,$s\]$s\$|]|" \
+        -e ":1;s|^\($s\)\($w\)$s:$s\[$s\(.*\)$s,$s\(.*\)$s\]|\1\2: [\3]\n\1  - \4|;t1" \
+        -e "s|^\($s\)\($w\)$s:$s\[$s\(.*\)$s\]|\1\2:\n\1  - \3|;p" $1 | \
+   sed -ne "s|,$s}$s\$|}|" \
+        -e ":1;s|^\($s\)-$s{$s\(.*\)$s,$s\($w\)$s:$s\(.*\)$s}|\1- {\2}\n\1  \3: \4|;t1" \
+        -e    "s|^\($s\)-$s{$s\(.*\)$s}|\1-\n\1  \2|;p" | \
    sed -ne "s|^\($s\):|\1|" \
+        -e "s|^\($s\)-$s[\"']\(.*\)[\"']$s\$|\1$fs$fs\2|p" \
+        -e "s|^\($s\)-$s\(.*\)$s\$|\1$fs$fs\2|p" \
         -e "s|^\($s\)\($w\)$s:$s[\"']\(.*\)[\"']$s\$|\1$fs\2$fs\3|p" \
-        -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|p"  $1 |
+        -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|p" | \
    awk -F$fs '{
       indent = length($1)/2;
       vname[indent] = $2;
-      for (i in vname) {if (i > indent) {delete vname[i]}}
+      for (i in vname) {if (i > indent) {delete vname[i]; idx[i]=0}}
+      if(length($2)== 0){  vname[indent]= ++idx[indent] };
       if (length($3) > 0) {
-         vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
-         printf("%s%s%s=\"%s\"\n", "'$prefix'",vn, $2, $3);
+         vn=""; for (i=0; i<indent; i++) { vn=(vn)(vname[i])("_")}
+         printf("%s%s%s=\"%s\"\n", "'$prefix'",vn, vname[indent], $3);
       }
    }'
 }
-
 # Get the variables
 eval $(parse_yaml ~/.stowpack/config)
 
+mkdir -p $stowpack_home/bowls
+if [ ! -d "$stowpack_home/bowls/main" ]; then
+  echo "Cooking main bowl."
+  if git clone https://github.com/stowpack/main $stowpack_home/bowls/main; then
+  echo "Cooked!"
+  else
+  echo "Error. Continuing without a main bowl."
+  fi
+fi
+
+# Parser for GitHub repository URLs
+function parse_tap_url {
+  # Assume the repo name or the full URL is passed as the first argument
+  input=$1
+  # Check if the input starts with http or https
+  if [[ $input =~ ^https?:// ]]; then
+    # Return the input as it is
+    echo $input
+  else
+    # Parse the input as a repo name
+    # Check if the input contains a slash
+    if [[ $input == *"/"* ]]; then
+      # Extract the user name and the repo name without the .git extension
+      user=$(echo $input | cut -d '/' -f 1)
+      name=$(echo $input | cut -d '/' -f 2 | cut -d '.' -f 1)
+    else
+      # Use "stowpack" as the default user name
+      user="stowpack"
+      # Extract the repo name without the .git extension
+      name=$(echo $input | cut -d '.' -f 1)
+    fi
+    # Construct the full url using https prefix
+    url="https://github.com/$user/bowl-$name.git"
+    # Print the url
+    echo $url
+  fi
+}
+
+# Yes or no
+function yes_no {
+  # Use read command with -n option to read only one character
+  # Use -p option to display the prompt message
+  # Use -t option to set a timeout in seconds
+  # Store the input in a variable called answer
+  read -n 1 -p "$1 [y/n]: " -t 10 answer
+  # Check the exit status of read command
+  # If it is not zero, it means either timeout or error occurred
+  # In that case, assume no as the default answer
+  if [ $? -ne 0 ]; then
+    answer=n
+  fi
+  # Convert the answer to lowercase
+  answer=${answer,,}
+  # Check if the answer is y or n
+  # Return 0 for yes, 1 for no
+  case $answer in
+    y) return 0 ;;
+    n) return 1 ;;
+    # If the answer is neither y nor n, print an error message and exit
+    *) echo "Invalid input: $answer" >&2; exit 2 ;;
+  esac
+}
+
 # Functions
+function stcli_update {
+  for folder in "$stowpack_home"/bowls/*; do
+    if [ -d "$folder" ]; then
+      echo "Updating $(basename $folder) tap..."
+      cd $folder
+      git pull
+    fi
+  done
+  echo "Updated!"
+}
+
 function stcli_install {
+  stcli_update
   # Loop through every folder in $stowpack_home/bowls
   for folder in "$stowpack_home"/bowls/*; do
     # Check if the folder is actually a folder
     if [[ -d "$folder" ]]; then
-      # Check if the folder name matches the argument
-      if [[ $(basename "$folder") == "$1" ]]; then
         # Check if the folder contains a file called stowpack.yaml
-        if [[ -f "$folder"/stowpack.yaml ]]; then
+        if [[ -f "$folder"/$1/stowpack.yaml ]]; then
           # Evaluate the output of parse_yaml as a bash script
-          eval "$(parse_yaml "$folder"/stowpack.yaml)"
+          eval "$(parse_yaml $folder/stowpack.yaml)"
           echo "$BOLD${1}:$RESET $stowpack_pkg_description"
+          if [ ! -d "$stowpack_home/tmp" ]; then
+             echo "No temp folder, creating one."
+             mkdir "$stowpack_home/tmp"
+          fi
+          echo "Downloading $stowpack_pkg_url"
+          SPID=$RANDOM
+          curl -o $stowpack_home/tmp/stowpack-$SPID "$stowpack_pkg_url"
+          if [ $stowpack_pkg_url == *.tar.gz ]; then
+             tar xzf $stowpack_home/tmp/stowpack-$SPID
+          elif [ $stowpack_pkg_url == *.zip ]; then
+             unzip $stowpack_home/tmp/stowpack-$SPID -d $stowpack_home/source/$1
+          else
+             echo "Error: incompatible format."
+             echo "Cleaning up..."
+             rm $stowpack_home/tmp/stowpack-$SPID
+             return 1
+          fi
+          for i in $stowpack_pkg_binlist_*; do
+             ln -s $stowpack_home/source/$1/$i $stowpack_home/bin/$i
+             echo "$i > "
+          done
+          rm $stowpack_home/tmp/stowpack-$SPID
           # Return the exit code of the last command
           return $?
         fi
       fi
-    fi
   done
   # If no results are found, echo a message and run stcli_search
   echo "Could not find $1. Similar matches:"
@@ -54,15 +154,7 @@ function stcli_install {
 }
 
 function stcli_help {
-    echo "                                                
-  -_-/    ,                                ,,   
- (_ /    ||        ;              _        ||   
-(_ --_  =||=  /'\\\\ \\\\/\\/\\ -_-_   < \\,  _-_ ||/\\ 
-  --_ )  ||  || || || | | || \\\\  /-|| ||   ||_< 
- _/  ))  ||  || || || | | || || (( || ||   || | 
-(_-_-    \\\\, \\\\,/  \\\\/\\\\/ ||-'   \\/\\\\ \\\\,/ \\\\,\\ 
-                          |/                    
-                          '                     " | lolcat || echo "                                                
+   echo "                                         
   -_-/    ,                                ,,   
  (_ /    ||        ;              _        ||   
 (_ --_  =||=  /'\\\\ \\\\/\\/\\ -_-_   < \\,  _-_ ||/\\ 
@@ -78,12 +170,14 @@ function stcli_help {
     echo -e "install\t\tinstall one or more packages."
     echo -e "uninstall\tuninstall one or more packages."
     echo -e "update\t\tupdate the repositories."
+    echo -e "cook\t\tcook a bowl, from GitHub or other git repo."
+    echo -e "update\t\tupdate taps to the latest version."
     echo -e "help\t\tprint this message."
 }
 
 # Check the number of arguments
 if [ $# -eq 0 ]; then
-    if [ $stowpack_require_command == "true" ]; then
+    if [ $stowpack_require_command = "true" ]; then
       echo "Please provide a command, or run \`stowpack help\` for help."
       exit 1
     fi
@@ -98,6 +192,11 @@ command=$1
 case $command in
     install)
         shift
+        if [ $# -eq 1 ]; then
+           echo "🠟 1 target to install."
+        else
+           echo "🠟 $# targets to install."
+        fi
         for pkg in $@; do
            stcli_install $pkg
         done
@@ -106,6 +205,22 @@ case $command in
     uninstall)
         stcli_uninstall $2
         ;;
+    cook)
+        shift
+        if [ $# -eq 1 ]; then
+           echo "🍜 1 bowl to cook."
+        else
+           echo "🍜 $# bowls to cook."
+        fi
+        for bowl in $@; do
+           echo "⊕ Cooking $bowl ($(parse_tap_url $bowl))"
+           git clone "$(parse_tap_url $bowl)" "$stowpack_home/bowls/$(basename $bowl)"
+        done
+        exit $?
+        ;;
+    update)
+         stcli_update
+         ;;
     help)
         stcli_help
         ;;
